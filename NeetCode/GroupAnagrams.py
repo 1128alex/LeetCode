5975e196a4bb4c0e26ec7fb42946b851d9a890b2
Alex Lee <1128alex@gmail.com>
2025-05-09 22:07:44 +0900

    2025-05-09

--- NeetCode/GroupAnagrams.py
+++ NeetCode/GroupAnagrams.py
@@ -5,22 +5,18 @@
 # An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
 
 # Example 1:
-
 # Input: strs = ["act","pots","tops","cat","stop","hat"]
-
 # Output: [["hat"],["act", "cat"],["stop", "pots", "tops"]]
+
 # Example 2:
-
 # Input: strs = ["x"]
-
 # Output: [["x"]]
+
 # Example 3:
-
 # Input: strs = [""]
-
 # Output: [[""]]
-# Constraints:
 
+# Constraints:
 # 1 <= strs.length <= 1000.
 # 0 <= strs[i].length <= 100
 # strs[i] is made up of lowercase English letters.
