f3c6a656c6f5e5e4d958f1e79b18f9f2d35746c0
Alex Lee <1128alex@gmail.com>
2026-02-01 22:42:17 -0700

    2026-02-01

--- #70_ClimbingStairs.cc
+++ #70_ClimbingStairs.cc
@@ -19,24 +19,12 @@
 // 2. 1 step + 2 steps
 // 3. 2 steps + 1 step
 
-class Solution
-{
+class Solution {
 public:
-    int climbStairs(int n)
-    {
-        if (n == 0 || n == 1)
-        {
+    int climbStairs(int n) {
+        if (n == 0 || n == 1) {
             return 1;
         }
-
-        vector<int> dp(n + 1);
-        dp[0] = 1;
-        dp[1] = 1;
-
-        for (int i = 2; i <= n; i++)
-        {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-        return dp[n];
+        return climbStairs(n-1) + climbStairs(n-2);
     }
 };
