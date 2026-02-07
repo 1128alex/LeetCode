aa6eb6b9deccfb070b0a64fbf919022c609d9f2c
Alex Lee <1128alex@gmail.com>
2026-02-07 10:00:52 -0700

    2026-02-07

---
+++ #67_AddBinary.cc
@@ -0,0 +1,37 @@
+// Given two binary strings a and b, return their sum as a binary string.
+
+// Example 1:
+// Input: a = "11", b = "1"
+// Output: "100"
+
+// Example 2:
+// Input: a = "1010", b = "1011"
+// Output: "10101"
+
+// Constraints:
+// 1 <= a.length, b.length <= 104
+// a and b consist only of '0' or '1' characters.
+// Each string does not contain leading zeros except for the zero itself.
+
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string res = "";
+        int temp = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+
+        while (i >= 0 || j >= 0 || temp) {
+            if (i >= 0) {
+                temp += a[i--] - '0';
+            }
+            if (j >= 0) {
+                temp += b[j--] - '0';
+            }
+            char t = temp % 2 + '0'; 
+            res = t + res;
+            temp /= 2;
+        }
+        return res;
+    }
+};
